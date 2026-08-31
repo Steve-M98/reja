a1cdd0f4a8c24cc13203f85662ab81b4bb73b203
Steve-M98 <shoyadbekmaxmudov@gmail.com>
2026-09-01 00:50:28 +0900

    feat: MongoDb Connected logic

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// MongoDb Call
+const db = require("./server").db();
+
+// 1: KIRISH CODE
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: SESSION CODE
+
+// 3:VIEWS CODE
+
+app.set("'views", "views");
+app.set("view engine", "ejs");
+
+// 4: ROUTING CODE
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,46 +1,30 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+let db;
+const connectionString =
+  "mongodb+srv://shoyadbekmaxmudov_db_user:auiqobY7z3MTk400@cluster0.kv3oghz.mongodb.net/Reja";
 
-// 1: KIRISH CODE
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2: SESSION CODE
-
-// 3:VIEWS CODE
-
-app.set("'views", "views");
-app.set("view engine", "ejs");
-
-// 4: ROUTING CODE
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
-  );
-});
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
+        );
+      });
+    }
+  },
+);
